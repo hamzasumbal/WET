5641713fb5a24628e2c50efb6385fa4c3473ddd0
hamzasumbal <msumbal.bee15seecs@seecs.edu.pk>
2021-05-20 02:06:08 +0500

    Sound added

---
+++ src/components/CTAButton.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, Dimensions, TouchableOpacity, Touchable } from 'react-native';
+import Colors from '../constants/Colors';
+import { Entypo } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+const WIDTH = Dimensions.get("screen").width;
+
+
+const CTAButton = ({play, setPlay, sound}) => {
+
+
+  /*   const [play, setPlay] = useState(false) */
+
+
+    return <TouchableOpacity style={[styles.container, {
+        backgroundColor: play ? Colors.shade : Colors.bg,
+        borderWidth: 3,
+        borderColor: !play ? Colors.shade : Colors.bg
+    }]}
+    onPress = {async ()=>{
+            if(play)
+            {
+               await sound.pauseAsync()
+            }
+            else {
+               await sound.playAsync()
+            }
+            setPlay(!play)
+    }}
+    >
+        {play ?
+            <AntDesign name="pause" size={38} color="black" />
+            :
+            <Entypo name="controller-play" size={38} color= {Colors.shade} style = {{
+                left : 3
+            }} />
+            }
+
+
+    </TouchableOpacity>
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        width: 70,
+        height: 70,
+        backgroundColor: Colors.shade,
+        borderRadius: 80,
+        alignSelf: "center",
+        marginTop: 60,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+});
+
+
+export default CTAButton;
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,29 +1,59 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { View, StyleSheet, Text, SafeAreaView, Dimensions, Animated } from 'react-native';
 import Colors from '../constants/Colors'
+import { Audio } from 'expo-av';
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
+import CTAButton from './CTAButton';
 const HEIGHT = Dimensions.get('screen').height
 
 
 const Timmer = () => {
     const Anim = useRef(new Animated.Value(0)).current;
+    const [sound, setSound] = useState();
+    const [key, setKey] = useState(0);
 
-    const [key, setKey] = useState(0)
+    const [play, setPlay] = useState(true)
 
-    const springAnim = async () => {
+    const springInAnim = async () => {
 
             Animated.spring(Anim, {
-                toValue: 1.2,
+                toValue: 1,
                 duration: 800,
                 useNativeDriver: false
             }).start(); // start the sequence group
-
-
     };
 
-    useEffect(()=>{
+    const springOutAnim = async () => {
 
-        springAnim()
+        Animated.timing(Anim, {
+            toValue: 0,
+            duration: 200,
+            useNativeDriver: false
+        }).start(); // start the sequence group
+
+};
+
+    async function playSound() {
+        console.log('Loading Sound');
+        const { sound } = await Audio.Sound.createAsync(
+           require('../../assets/water-eject.mp3')
+        );
+        setSound(sound);
+    
+        console.log('Playing Sound');
+        await sound.playAsync(); }
+    
+      useEffect(() => {
+        return sound
+          ? () => {
+              console.log('Unloading Sound');
+              sound.unloadAsync(); }
+          : undefined;
+      }, [sound]);
+
+    useEffect(()=>{
+        springInAnim()
+        playSound()
     },[])
 
 
@@ -34,26 +64,29 @@ const Timmer = () => {
     <CountdownCircleTimer
                     key={key}
                     trailColor={Colors.accent}
-                    isPlaying
-                    duration={15}
+                    isPlaying = {play}
+                    duration={10}
+                    size = {230}
                     colors={[
                         [Colors.shade, 0.4],
                     ]}
 
                     onComplete={() => {
-                        setKey(prevKey => prevKey + 1)
+                        /* setKey(prevKey => prevKey + 1) */
+                        springOutAnim()
 
                     }}
                 >
                     {({ remainingTime, animatedColor }) => {
 
                         return <Animated.Text style={{
-                            color: Colors.shade, fontSize: 60, fontWeight: 'bold'
+                            color: Colors.shade, fontSize: 70, fontWeight: 'bold'
                         }}>
                             {remainingTime}
                         </Animated.Text>
                     }}
                 </CountdownCircleTimer>
+                <CTAButton play = {play} setPlay = {setPlay} sound = {sound}/>
                 </Animated.View>
 
 };
