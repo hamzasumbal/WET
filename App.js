ecf0d7aadec0462a62f1c5b436ab5c094886dafb
hamzasumbal <msumbal.bee15seecs@seecs.edu.pk>
2021-05-21 02:47:22 +0500

    Drawer Updated

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { View, Text , Dimensions} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator,DrawerContentScrollView,DrawerItemList, } from '@react-navigation/drawer';
 import HomeScreen from './src/screens/HomeScreen';
 import SettingScreen from './src/screens/SettingScreen';
 import PlayScreen from './src/screens/PlayScreen';
+import Colors from './src/constants/Colors';
 
 
 const WIDTH = Dimensions.get('screen').width
@@ -13,16 +14,45 @@ const WIDTH = Dimensions.get('screen').width
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <View style = {{
+        height : 200,
+        width : WIDTH * 0.5,
+        borderWidth : 2,
+        alignSelf : "center",
+        backgroundColor : "red"
+      }}> 
+
+      </View>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
+
 function MyDrawer() {
   return (
     <Drawer.Navigator
+    drawerContent = {CustomDrawerContent}
     drawerPosition = {'right'}
     drawerStyle={{
 	    width: WIDTH * 0.6,
+      backgroundColor : Colors.accent
 	  }}
+    drawerContentOptions = {
+      {
+        labelStyle : {
+          color : Colors.text,
+          fontWeight: "700"
+        },
+      }
+    }
     >
       <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Setting" component={SettingScreen} />
+      <Drawer.Screen name="Setting" component={SettingScreen} options = {{
+        title : "About the Developer"
+      }}/>
     </Drawer.Navigator>
   );
 }
