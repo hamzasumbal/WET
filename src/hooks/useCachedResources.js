d1ca8e6465fb4f08910f84a818b2cd68b4e0104e
hamzasumbal <msumbal.bee15seecs@seecs.edu.pk>
2021-05-25 05:46:14 +0500

    small error handling

--- src/hooks/useCachedResources.js
+++ src/hooks/useCachedResources.js
@@ -6,56 +6,58 @@ import ThemeContext from '../contexts/ThemeContext'
 
 function cacheImages(images) {
     return images.map(image => {
-      if (typeof image === 'string') {
-        return Image.prefetch(image);
-      } else {
-        return Asset.fromModule(image).downloadAsync();
-      }
+        if (typeof image === 'string') {
+            return Image.prefetch(image);
+        } else {
+            return Asset.fromModule(image).downloadAsync();
+        }
     });
-  }
+}
 
 export default function useCachedResources() {
 
     const [loadingComplete, setLoadingComplete] = useState(false)
-    const {state : theme, changeTheme, isThemeStored} = useContext(ThemeContext)
+    const { state: theme, changeTheme, isThemeStored } = useContext(ThemeContext)
 
     useEffect(() => {
-         async function loadResourcesAndDataAsync() {
+        async function loadResourcesAndDataAsync() {
 
             await SplashScreen.preventAutoHideAsync();
-            if(!await isThemeStored())
-            {
+            if (!await isThemeStored()) {
                 const colorScheme = Appearance.getColorScheme();
                 if (colorScheme === 'dark') {
-                   await  changeTheme("dark")
-                }else if (colorScheme === "light"){
+                    await changeTheme("dark")
+                } else if (colorScheme === "light") {
                     await changeTheme("light")
+                }
+                else {
+                    await changeTheme('dark')
                 }
 
             }
 
 
-           try {
-     
-             // Load images
-             const imageAssets = cacheImages([
-                 require('../../assets/phoneback.png'),
-                 require('../../assets/phonefront.png'),
-                 require('../../assets/profileimage.jpg'),
-                 require('../../assets/logo.png'),
-             ]);
-     
-             await Promise.all([...imageAssets]);
-           } catch (e) {
-             console.warn(e);
-           } finally {
-             setLoadingComplete(true);
-           }
-         }
+            try {
+
+                // Load images
+                const imageAssets = cacheImages([
+                    require('../../assets/phoneback.png'),
+                    require('../../assets/phonefront.png'),
+                    require('../../assets/profileimage.jpg'),
+                    require('../../assets/logo.png'),
+                ]);
+
+                await Promise.all([...imageAssets]);
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                setLoadingComplete(true);
+            }
+        }
 
         loadResourcesAndDataAsync();
 
-           
+
 
 
 
